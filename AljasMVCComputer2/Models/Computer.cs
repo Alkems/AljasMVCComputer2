77205ab93e6dad523c7556c464796bad7f43b78d
Alkems <mynamezalter@gmail.com>
2022-09-23 01:12:11 +0300

    Added validations

--- AljasMVCComputer2/Models/Computer.cs
+++ AljasMVCComputer2/Models/Computer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +10,15 @@ namespace AljasMVCComputer2.Models
     public class Computer
     {
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Specs { get; set; }
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
